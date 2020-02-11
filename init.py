a3e8e628692d6e97ebeed617c45c251a64d736bb
chaitanya2692 <chaitanya2692@gmail.com>
2020-02-11 13:48:31 -0800

    init

---
+++ init.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+from mpl_toolkits.basemap import Basemap
+
+
+def plot_on_map():
+    return
+
+#Feature Engineering (add column)
+def route_distance():
+    return    
+
+
+if __name__ == "__main__":
+    df_1 = pd.read_csv('trackdata_c1.csv')
+    df_2 = pd.read_csv('trackdata_c2.csv')
+    df_x = pd.read_csv('trackdata_cx.csv')

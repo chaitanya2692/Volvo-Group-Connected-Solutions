ce9e1ffb75ad0e159162f3d0aab29fbe71408bfa
chaitanya2692 <chaitanya2692@gmail.com>
2020-02-12 22:44:58 +0100

    made geo_dist calc func

--- init.py
+++ init.py
@@ -6,8 +6,15 @@ This is a temporary script file.
 """
 
 import pandas as pd
+import numpy as np
+#import herepy
+#from herepy import RoutingApi
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+from geopy import distance
+from multiprocessing import Process, Manager
+import pyproj as proj
+import json
 
 def plot_on_map(df):
 
@@ -32,19 +39,93 @@ def plot_on_map(df):
     # df['lat_lon'] = list(zip(df.Easting, df.Northing)) # Creating tuples
     # df_2000 = df[df['AADFYear']==2000]
     
-    for x,y in zip(df['lat'],df['lon']):
-        m.plot(x, y, marker = 'o', c='r', markersize=1, alpha=0.8)
-    
-    plt.savefig('test.png')
+    x,y=m(df['lon'].tolist(),df['lat'].tolist())
+    m.plot(x,y,'r*',markersize=5)
+#    plt.savefig('test.png')
+
     return
 
-#Feature Engineering (add column)
-def route_distance():
-    return    
+#Feature Engineering (Works)
+def route_distance(df, uid):
+    lon = df_1.loc[df['uid']==uid, 'lon'].to_numpy()
+    lat = df_1.loc[df['uid']==uid, 'lat'].to_numpy()
+    total_dist = 0
+    for i in range(len(lon)-1):
+        dist = distance.distance((lat[i],lon[i]),(lat[i+1],lon[i+1])).km
+        total_dist = dist + total_dist
+    
+#        response = RoutingApi.truck_route(lat,
+#                                      lon,
+#                                      [herepy.RouteMode.truck, herepy.RouteMode.fastest])
+    return_dict[uid] = total_dist
 
+#def check_time_period(df):
+#    periodlist = []
+#    for i in uid:
+#        timestamps = max(df_1.loc[df_1['uid']==i, 'row_number'].to_numpy())
+#        timeperiod = route_distance/timestamps 
+#        periodlist.append(timeperiod)
+    
+#Shit doesn't work
+def cood_trans(lat,lon):
+    # setup your projections
+    crs_wgs = proj.Proj(init='epsg:4326') # assuming you're using WGS84 geographic
+    crs_bng = proj.Proj(init='epsg:27700') # use a locally appropriate projected CRS
+    
+    # then cast your geographic coordinate pair to the projected system
+    x, y = proj.transform(crs_wgs, crs_bng, lon, lat)
+    return x,y
+
+#Shit doesn't work
+def norm_route_distance(df, uid):
+    lon = df_1.loc[df['uid']==uid, 'lat'].to_numpy()
+    lat = df_1.loc[df['uid']==uid, 'lon'].to_numpy()
+    points = []
+    for la,lo in zip(lat,lon):
+        lat_x,lon_y = cood_trans(la,lo)
+        points.append((lat_x,lon_y))
+    d = np.diff(points, axis=0)
+    segdists = np.hypot(d[:,0], d[:,1])
+#    segdists = np.sqrt((d ** 2).sum(axis=1))
+    return segdists
+    
+def get_uid(df):
+    uid = set(df['uid'].tolist())
+    return list(uid)
 
 if __name__ == "__main__":
     df_1 = pd.read_csv('trackdata_c1.csv')
-    df_2 = pd.read_csv('trackdata_c2.csv')
+#    df_2 = pd.read_csv('trackdata_c2.csv')
+    
+    # Needs sorting according to row_number (currently coods scrambled)
     df_x = pd.read_csv('trackdata_cx.csv')
-    plot_on_map(df_1)
+#    lon = df_1.loc[df_1['uid']==5, 'lat'].to_numpy()
+#    lat = df_1.loc[df_1['uid']==5, 'lon'].to_numpy()
+    
+#    uid = route_distance(df, uid)
+    manager = Manager()
+    return_dict = manager.dict()
+    jobs=[]
+    
+    '''Run route_distance in Colab'''
+    trucks = sorted(set(df_1['uid']))
+    
+    #Parallel Implementation
+    for truck in trucks[:2]:
+        m = Process(target=route_distance, name=truck, args=(df_x, truck))
+        m.daemon = True
+        jobs.append(m)
+        m.start()
+    
+    for proc in jobs:
+        proc.join()
+    
+    distances = return_dict.copy()
+    
+#    distance_1 = {}
+#    for truck in trucks[:30]:
+#        distance_1[truck] = np.sum(norm_route_distance(df_x, truck))
+    
+    with open('geodesic_distances_c1.txt') as json_file:
+        data = json.load(json_file)
+#    plot_on_map(df_1)

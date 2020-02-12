43480f4b8e67c671137a7e233ec95f3f08c4b372
Chaitanya Kudaka <chakud@puff.infotiv.se>
2020-02-12 17:18:36 +0100

    basemap plot crashes on high resolution

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Volvo-Group-Connected-Solutions
 
 Dataset - https://drive.google.com/drive/folders/1qaI2TA5WAoyJ6NyRnDzzEcCr6mut5hFb?usp=sharing
+
+Install Basemap - conda install basemap 
+Install proj4 - conda install proj4 (5.2.0)
+Install high qquality maps - conda install basemap-data-hires
+
--- init.py
+++ init.py
@@ -7,9 +7,35 @@ This is a temporary script file.
 
 import pandas as pd
 from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
 
+def plot_on_map(df):
 
-def plot_on_map():
+    # How much to zoom from coordinates (in degrees)
+    zoom_scale = 1
+    
+    # Setup the bounding box for the zoom and bounds of the map
+    bbox = [df['lat'].min()-zoom_scale,df['lat'].max()+zoom_scale,\
+            df['lon'].min()-zoom_scale,df['lon'].max()+zoom_scale]
+    
+    plt.figure(figsize=(12,6))
+    # Define the projection, scale, the corners of the map, and the resolution.
+    m = Basemap(projection='merc',llcrnrlat=bbox[0],urcrnrlat=bbox[1],\
+    llcrnrlon=bbox[2],urcrnrlon=bbox[3],lat_ts=10,resolution='i')
+
+    # m.drawmapboundary(fill_color='#46bcec') # Make your map into any style you like
+    # m.fillcontinents(color='#f2f2f2',lake_color='#46bcec') # Make your map into any style you like
+    m.drawcoastlines()
+    # m.drawrivers() # Default colour is black but it can be customised
+    m.drawcountries()
+    
+    # df['lat_lon'] = list(zip(df.Easting, df.Northing)) # Creating tuples
+    # df_2000 = df[df['AADFYear']==2000]
+    
+    for x,y in zip(df['lat'],df['lon']):
+        m.plot(x, y, marker = 'o', c='r', markersize=1, alpha=0.8)
+    
+    plt.savefig('test.png')
     return
 
 #Feature Engineering (add column)
@@ -21,3 +47,4 @@ if __name__ == "__main__":
     df_1 = pd.read_csv('trackdata_c1.csv')
     df_2 = pd.read_csv('trackdata_c2.csv')
     df_x = pd.read_csv('trackdata_cx.csv')
+    plot_on_map(df_1)
